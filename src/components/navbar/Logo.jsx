ebb30b3a886abaa55869d1a106f77249ac52c1fa
yan05021 <justyiyan@gmail.com>
2023-06-28 16:44:08 +0200

    script database

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
-  return (
-    <Link to="/">
-      <div className="font-monoton text-3xl hover:text-red-800 cursor-pointer text-center transition">
-        eyesome
-      </div>
-    </Link>
-  );
+    return (
+        <Link to="/">
+            <div className="font-monoton text-3xl hover:text-red-800 cursor-pointer text-center transition">
+                emmaus
+            </div>
+        </Link>
+    );
 };
 
 export default Logo;
