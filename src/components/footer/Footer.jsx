bbfe173f46f751fa240b734484745c0c335b1320
Vincent DESBROSSES <vincedsb@gmail.com>
2023-06-29 16:25:15 +0200

    vincent08 - Home cards + favicon + footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,16 +8,16 @@ import {
 const Footer = () => {
   return (
     <div className="py-5 mt-3  bg-amber-50 flex flex-wrap justify-center items-center gap-2 md:gap-10  absolute right-0 left-0 ">
-      <p>Eyesome made with 💜 by Sandhya | Edited by Hackathon Team of Nantes WCS Campus</p>
+      <p>Edited by Hackathon Team of Nantes WCS Campus</p>
       <p className="flex gap-3">
-        <a href="https://github.com/SandhyaR1007">
+        <a href="https://github.com/vincedsb1/hackathon2">
           <AiFillGithub className="text-2xl text-gray-800" />
         </a>
-        <a href="https://www.linkedin.com/in/sandhya-rajwanshi-a75b331b4/">
+        <a href="https://www.linkedin.com/in/vincent-desbrosses/">
           {" "}
           <AiOutlineLinkedin className="text-2xl text-gray-800" />
         </a>
-        <a href="https://twitter.com/SandhyaR1007">
+        <a href="https://twitter.com/vincedsb">
           <AiOutlineTwitter className="text-2xl text-gray-800" />
         </a>
       </p>
